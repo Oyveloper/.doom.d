2e2cc2c1f8056ff4fc9a1314ecec5ce2800bf763
Øyvind Monsen <yvind.monsen@gmail.com>
2020-04-24 16:42:21 +0200

    added snippets and improved org agenda stuff

--- config.el
+++ config.el
@@ -20,6 +20,9 @@
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "monospace" :size 14))
 
+(use-package avy
+  :ensure t)
+
 (setq mac-option-modifier nil
       mac-command-modifier 'meta)
 
@@ -49,6 +52,35 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+
+(setq org-agenda-files '("~/org/todo"))
+(setq +org-capture-todo-file "~/org/todo/todo.org")
+
+(after! org
+    (setq org-capture-templates
+          '(("t" "Personal todo" entry
+            (file+headline +org-capture-todo-file "Inbox")
+            "* TODO %?\n%i\n%a" :prepend t)
+            ("n" "Personal notes" entry
+            (file+headline +org-capture-notes-file "Inbox")
+            "* %u %?\n%i\n%a" :prepend t)
+            ("j" "Journal" entry
+            (file+olp+datetree +org-capture-journal-file)
+            "* %U %?\n%i\n%a" :prepend t)
+            ("p" "Templates for projects")
+            ("pt" "Project-local todo" entry
+            (file+headline +org-capture-project-todo-file "Inbox")
+            "* TODO %?\n%i\n%a" :prepend t)
+            ("pn" "Project-local notes" entry
+            (file+headline +org-capture-project-notes-file "Inbox")
+            "* %U %?\n%i\n%a" :prepend t)
+            ("pc" "Project-local changelog" entry
+            (file+headline +org-capture-project-changelog-file "Unreleased")
+            "* %U %?\n%i\n%a" :prepend t)
+            ("o" "Centralized templates for projects")
+            ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+            ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+            ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t))))
 
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
